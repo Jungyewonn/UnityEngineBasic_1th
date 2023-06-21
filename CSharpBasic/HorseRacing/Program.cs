023f2b5b04f30757c1185974d40adafc2fcf46e2
Jungyewonn <101615099+Jungyewonn@users.noreply.github.com>
2023-06-21 22:16:46 +0900

    C#

---
+++ CSharpBasic/HorseRacing/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+
+namespace HorseRacing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Horse[] horses = new Horse[5];
+            for (int i = 0; i < horses.Length; i++)
+            {
+                horses[i].name = $"경주마{i}";
+            }
+            string[] rankArray = new string[5];
+            int currentGrade = 0;
+
+            while (currentGrade < horses.Length)
+            {
+                //달리는 내용 작성
+
+                Thread.Sleep(1000);
+            }
+
+            for (int i = 0; i < rankArray.Length; i++)
+            {
+                Console.WriteLine($"{rankArray[i]} 가 {i + 1} 등");
+            }
+        }
+    }
+}
