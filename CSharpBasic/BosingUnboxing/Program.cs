199793c3faee4396df16863090c875424a540be4
Jungyewonn <101615099+Jungyewonn@users.noreply.github.com>
2023-06-28 21:54:43 +0900

    C#

---
+++ CSharpBasic/BosingUnboxing/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace BosingUnboxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object obj1 = new object();
+            object obj2 = new object();
+            obj1 = 1;
+            int a = 1;
+            long long1 = (long)obj1;
+            int b = 1;
+
+            // boxing
+            // object 타입으로 변환하는 과정 (힙영역에 object 타입 객체 만들고 데이터 씀)
+
+            // unboxing
+            // 힙영역의 object 객체에서 데이터를 원래 데이터로 바꾸는 과정
+            Console.WriteLine(Compare(1, 1));
+        }
+
+        //static bool Compare(int a, int b)
+        //{
+        //    return a == b;
+        //}
+        static bool Compare(object a, object b)
+        {
+            return (int)a == (int)b;
+        }
+    }
+}
--- CSharpBasic/InHeritance/Character.cs
+++ CSharpBasic/InHeritance/Character.cs
@@ -9,8 +9,27 @@ namespace InHeritance
     // 추상멤버를 가지고있다.
     // -> 해당 멤버를 가지는 클래스가 인스턴스화 가능하다면 해당 함수를 호출할수 있어야하는데 모순이 생긴다.
     // -> 추상 멤버를 가지고있으면, 클래스 마찬가지로 추상 클래스여야한다.
-    internal abstract class Character : Creature
+    internal abstract class Character : Creature, IDamageable
     {
+        private float _hp;
+
+        public float GetHp()
+        {
+            return _hp;
+        }
+
+        public float SetHp(float value)
+        {
+            if (value < 0)
+                value = 0;
+
+            _hp = value;
+        }
+        public void Damage(float amount)
+        {
+            _hp = amount;
+        }
+
         public abstract void UniqueSkill(); // 추상함수는 정의하지않고 선언만 함.
 
     }
